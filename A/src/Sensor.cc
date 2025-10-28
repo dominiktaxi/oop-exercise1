0ba3f7f5df20093aeb45f58cb38842ec978b5636
Daniel Shamany <dshamany@proton.me>
2025-10-28 16:11:59 +0100

    bugfix: changed from SensorRead to SensorReading

--- A/src/Sensor.cc
+++ A/src/Sensor.cc
@@ -1,16 +1,12 @@
 #include "Sensor.h"
 
-Sensor::Sensor(const std::string& name) : _name(name) {}
+Sensor::Sensor(const std::string &name) : _name(name) {}
 
-SensorRead Sensor::read() const
-{
-    SensorRead read;
-    read.value = 1337;
-    read.timestamp = "12/12/12";
-    return read;
+SensorReading Sensor::read() const {
+  SensorReading read;
+  read.value = 1337;
+  read.timestamp = "12/12/12";
+  return read;
 }
 
-std::string Sensor::name()
-{
-    return _name;
-}
+std::string Sensor::name() { return _name; }
