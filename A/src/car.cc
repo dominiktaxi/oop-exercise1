ecac3003a94f13f3aa2e07a406ca343d345b22b9
Dominik Kalinski <dominik.kalinski@hotmail.com>
2025-10-30 10:51:48 +0100

    Add parking house and car classes

---
+++ A/src/car.cc
@@ -0,0 +1,10 @@
+#include "car.h"
+
+Car::Car( const std::string& regNumber, const std::string& arrivalTime) : regNumber(regNumber), arrivalTime(arrivalTime)
+{}
+
+void Car::print() const
+{
+    std::cout << "Car with regNumber: " << regNumber << " arrived at " << arrivalTime << std::endl;
+}
+
